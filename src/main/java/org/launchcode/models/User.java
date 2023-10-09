42c5bc57768ff6e6dbffc01ed9e6034e32643c85
Jialing Liu <jialingl.liu@gmail.com>
2023-10-09 17:54:07 -0400

    Finished studio ch. 15 on validation

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,8 +1,20 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank (message = "username is required.")
+    @Size(min = 5, max = 15, message = "username must be between 5-15 characters.")
     private String username;
+
+    @Email (message = "add valid email.")
     private String email;
+
+    @NotBlank (message = "password is required.")
+    @Size(min = 6, message = "password is at least 6 characters long.")
     private String password;
 
     public User() {
